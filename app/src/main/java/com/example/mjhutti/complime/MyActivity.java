9e49e7bb8a2e30935ec5d8945ff13d893b60bd13
Mandeep Jhutti <crazymon1@gmail.com>
2014-12-11 16:56:10 +0000

    Added ability to view account email

--- app/src/main/java/com/example/mjhutti/complime/MyActivity.java
+++ app/src/main/java/com/example/mjhutti/complime/MyActivity.java
@@ -1,9 +1,16 @@
 package com.example.mjhutti.complime;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 
 public class MyActivity extends Activity {
@@ -11,9 +18,27 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Pattern emailPattern = Patterns.EMAIL_ADDRESS; // API level 8
+        Account[] accounts = AccountManager.get(getApplicationContext()).getAccountsByType("com.google");
+        for (Account account : accounts) {
+            if (emailPattern.matcher(account.name).matches()) {
+                String possibleEmail = account.name;
+                Toast toast = Toast.makeText(getApplicationContext(), possibleEmail, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+
+        }
+
+
         setContentView(R.layout.activity_my);
     }
 
+
+    public void pickAccount(){
+        //User needs ability to pick the correct gmail account perhaps if they have more than one one on their phone
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
